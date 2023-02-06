ffcd253b753071027e50952e8d7eab8ab202a88f
Emmanuel Zambrano <Emmanuelleones3@gmail.com>
2023-02-05 22:51:36 -0400

    Manejador de eventos 1.2: Se procedio a realizar eventos en los componentes: CreateTodoButton, TodoItem y TodoSearch

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import "./TodoItemStyles.css";
 
-const TodoItem = ({text}) =>{
+const TodoItem = ({text}) => {
+    const onComplete = () => {
+        console.log ('Completaste la tarea: '+ text);
+    }
+    const onDelete = () => {
+        console.log ('Borrarte la tarea: '+ text);
+    }
     return(
         <React.Fragment>
-            <li className="listTask">
-                <span>
-                    <p>{text}</p>
+            <li className="TodoItem">
+                <span className={`Icon Icon-check ${text.completed && 'Icon-check--active'}`} onClick={()=> onComplete({text})}>
+                        √
+                </span>
+                <p className={`TodoItem-p ${text.completed && 'TodoItem-p--complete'}`}>
+                        {text}
+                </p>
+                <span className={`Icon Icon-check ${text.delete && 'Icon-check--active'}`} onClick={()=> onDelete({text})}>
+                        X
                 </span>
             </li>
         </React.Fragment>
--- src/components/TodoSearch/TodoSearch.jsx
+++ src/components/TodoSearch/TodoSearch.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import "./TodoSearchStyles.css"
 
 const TodoSearch = () =>{
+    const onSearchValueChange = (e) =>{ // Función que busca el valor
+        console.log (e.target.value)
+    }
     return(
         <React.Fragment>
-            
+            <input
+            placeholder="Tarea"
+            onChange={onSearchValueChange } // Investiga la función del evento onChange           
+            />
         </React.Fragment>
     );
 }
-
 export {TodoSearch};
